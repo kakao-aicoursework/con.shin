0acdeb9bfc5be5f3bddae541b2adb785a137cac0
con.shin <con.shin@kakaocorp.com>
2023-09-19 16:57:06 +0900

    api_key 제거

--- chatbot/chatbot/chatbot.py
+++ chatbot/chatbot/chatbot.py
@@ -10,7 +10,6 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain.chains import LLMChain
 
-os.environ["OPENAI_API_KEY"] = "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"
 # openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 def read_prompt_template(file_path: str) -> str:
