46e1705ede37c8c9ded21281da9cf8fd5d95e2bf
con.shin <con.shin@kakaocorp.com>
2023-09-19 16:48:55 +0900

    카카오 싱크 어시스트 기능 추가

--- chatbot/chatbot/chatbot.py
+++ chatbot/chatbot/chatbot.py
@@ -1,26 +1,36 @@
 """Welcome to Pynecone! This file outlines the steps to create a basic app."""
 
 # Import pynecone.
-import openai
+# import openai
 import os
 import pynecone as pc
 from pynecone.base import Base
 
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.chains import LLMChain
 
-# openai.api_key = "<YOUR_OPENAI_API_KEY>"
-openai.api_key = os.environ.get('OPEN_API_KEY')
+os.environ["OPENAI_API_KEY"] = "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"
+# openai.api_key = os.environ.get('OPENAI_API_KEY')
 
-def question_to_chatgpt(text) -> str:
-    messages = [{"role": "user", "content": text}]
+def read_prompt_template(file_path: str) -> str:
+    with open(file_path, "r") as f:
+        prompt_template = f.read()
 
-    # API 호출
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                            messages=messages)
-    translated_text = response['choices'][0]['message']['content']
-    # Return
-    return translated_text
+    return prompt_template
 
+def question_to_chatgpt(question: str) -> str:
+    writer_llm = ChatOpenAI(temperature=1, max_tokens=8192, model='gpt-3.5-turbo-16k')
+    writer_prompt_template = ChatPromptTemplate.from_template(
+        template=read_prompt_template('project_data_카카오싱크.txt'))
+    writer_chain = LLMChain(llm=writer_llm, prompt=writer_prompt_template, output_key='output')
 
+    result = writer_chain(dict(
+        question=question,
+    ))
+
+    return result['output']
+    
 class Message(Base):
     role: str
     align: str
@@ -35,6 +45,7 @@ class AssistMessage(Message):
 
 class State(pc.State):
     """The app state."""
+    is_working: bool = False
     text: str = ""
     messages: list[Message] = []
     question: list[str] = []
@@ -44,10 +55,14 @@ class State(pc.State):
     def output(self) -> str:
         if not self.text.strip():
             return "Translations will appear here."
+        
         answer = question_to_chatgpt(self.text)
         return answer
 
-    def post(self):
+    async def post(self):
+        self.is_working = True
+        yield
+
         self.messages = self.messages + [
             UserMessage(
                 role='user',
@@ -60,7 +75,9 @@ class State(pc.State):
                 text=self.output,
             )
         ]
+        self.is_working = False
         self.text = ""
+        yield
 
 
 # Define views.
@@ -76,49 +93,22 @@ def header():
     )
 
 
-def down_arrow():
-    return pc.vstack(
-        pc.icon(
-            tag="arrow_down",
-            color="#666",
-        )
-    )
-
-
-def text_box(text):
-    return pc.text(
-        text,
-        background_color="#fff",
-        padding="1rem",
-        border_radius="8px",
-    )
-
-
 def message(message: Message):
     return pc.box(
-        pc.text(message.title),
         pc.text(
+            message.title, 
+            font_weight="bold",
+            font_size="2em",
+        ),
+        pc.markdown(
             message.text
         ),
-        text_align=message.align,
         width="100%",
         background_color="#f5f5f5",
         padding="1rem",
         border_radius="8px",
     )
 
-    
-
-
-def smallcaps(text, **kwargs):
-    return pc.text(
-        text,
-        font_size="0.7rem",
-        font_weight="bold",
-        text_transform="uppercase",
-        letter_spacing="0.05rem",
-        **kwargs,
-    )
 
 def assist():
     return pc.box(
@@ -133,30 +123,17 @@ def assist():
         )
     )
     
-def output():
-    return pc.box(
-        pc.box(
-            smallcaps(
-                "Output",
-                color="#aeaeaf",
-                background_color="white",
-                padding_x="0.1rem",
-            ),
-            position="absolute",
-            top="-0.5rem",
-        ),
-        pc.text(State.output),
-        padding="1rem",
-        border="1px solid #eaeaef",
-        margin_top="1rem",
-        border_radius="8px",
-        position="relative",
-    )
-
 
 def index():
     """The main view."""
     return pc.container(
+        pc.cond(State.is_working,
+                    pc.spinner(
+                        color="lightgreen",
+                        thickness=5,
+                        speed="1.5s",
+                        size="xl",
+                    ),),
         header(),
         pc.vstack(
             pc.foreach(State.messages, message),
@@ -166,11 +143,11 @@ def index():
         ),
         assist(),
         padding="2rem",
-        max_width="600px"
+        max_width="1024px"
     )
 
 
 # Add state and page to the app.
 app = pc.App(state=State)
-app.add_page(index, title="Translator")
+app.add_page(index, title="카카오싱크 어시스트")
 app.compile()
